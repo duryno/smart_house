d58b2dd1ad14e0a0a380ce97652d3f6ccf1023b9
Juraj <Juraj@Dury-Lenovo>
2016-01-19 17:22:28 +0100

    Commit before the final presentation

--- src/main/java/smartHouse/Notification/SseBroadcasterResource2.java
+++ src/main/java/smartHouse/Notification/SseBroadcasterResource2.java
@@ -14,6 +14,7 @@ import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
 import smartHouse.resourceInterfaces.SseBroadcasterResourceInterface2;
 
+
 /**
  *
  * @author Calle
